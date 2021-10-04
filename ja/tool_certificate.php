11987adf15888f2f32cacb04867a84d37ca0bd62
Marina Glancy <marina@moodle.com>
2021-10-04 11:00:20 +0200

    ja

---
+++ ja/tool_certificate.php
@@ -0,0 +1,187 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_certificate', language 'ja_wp', version '3.11'.
+ *
+ * @package     tool_certificate
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addcertpage'] = 'ページを追加';
+$string['addelement'] = '要素を追加';
+$string['addelementwithname'] = '\'{$a}\' 要素を追加';
+$string['aissueswerecreated'] = '{$a} 枚発行されました';
+$string['aligncentre'] = '中央';
+$string['alignleft'] = '左';
+$string['alignment'] = 'テキストの配置';
+$string['alignment_help'] = 'テキストの右揃えでは、要素の組み合わせ(ポジションXとポジションY)がテキストボックス右上に配置されます。中央揃えでは中央に、左揃えでは左上に配置されます。';
+$string['alignright'] = '右';
+$string['availableincourses'] = 'サブカテゴリとコースで利用可能';
+$string['availableincourses_help'] = 'このオプションを有効にすると、（発行機能を持つ）ユーザーは、選択したカテゴリ内のすべてのコースとそのサブカテゴリ内のコースでこのテンプレートを使用できるようになります。このオプションが無効になっている場合、このテンプレートは、選択したカテゴリの発行機能を持つユーザーのみが使用できます。';
+$string['certificate'] = '認定証';
+$string['certificate:image'] = '認定証イメージを管理';
+$string['certificate:issue'] = 'ユーザに認定証を発行する';
+$string['certificate:manage'] = '認定証を管理';
+$string['certificate:verify'] = '全ての認定証を確認する';
+$string['certificate:viewallcertificates'] = '発行されたすべての認定証とテンプレートを表示する';
+$string['certificate_customfield'] = '認定証のカスタムフィールド';
+$string['certificatecopy'] = '{$a} (コピー)';
+$string['certificateimages'] = '認定証のイメージ';
+$string['certificates'] = '認定証';
+$string['certificatesissued'] = '認定証発行日';
+$string['certificatesissues'] = '認定証の発行';
+$string['certificatetemplate'] = '認定証テンプレート';
+$string['certificatetemplatename'] = '認定証テンプレート名';
+$string['certificatetemplates'] = '認定証テンプレート';
+$string['changeelementsequence'] = '前へまたは後ろへ';
+$string['code'] = 'コード';
+$string['codewithlink'] = 'リンクつきコード';
+$string['coursecategorywithlink'] = 'リンクつきコースカテゴリ';
+$string['createtemplate'] = '新しいテンプレート';
+$string['customfield_previewvalue'] = '値のプレビュー';
+$string['customfield_previewvalue_help'] = '認定証テンプレートをプレビューするときに表示される値';
+$string['customfield_visible'] = '表示';
+$string['customfield_visible_help'] = '認定証テンプレートでのこのフィールドの選択を許可';
+$string['customfieldsettings'] = '共通する認定証のカスタムフィールド設定';
+$string['deleteelement'] = '要素を削除';
+$string['deleteelementconfirm'] = '本当に要素 \'{$a}\' を消去しますか？';
+$string['deletepage'] = 'ページを削除';
+$string['deletepageconfirm'] = '本当にこの認定証ページを消去しますか？';
+$string['deletetemplateconfirm'] = '本当にこの認定証テンプレート \'{$a}\' とそれに関連するデータを消去しますか？この操作は元に戻せません。';
+$string['demotmpl'] = '認定証デモテンプレート';
+$string['demotmplawardedon'] = '授与';
+$string['demotmplawardedto'] = 'この認定証はに授与されます';
+$string['demotmplbackground'] = '背景画像';
+$string['demotmplcoursefullname'] = 'コースのフルネーム';
+$string['demotmpldirector'] = '校長';
+$string['demotmplforcompleting'] = 'コース修了用';
+$string['demotmplissueddate'] = '発行日';
+$string['demotmplqrcode'] = 'QRコード';
+$string['demotmplsignature'] = '署名';
+$string['demotmplusername'] = 'ユーザー名';
+$string['duplicate'] = '複製';
+$string['duplicatetemplateconfirm'] = '本当にテンプレート \'{$a}\' を複製しますか？';
+$string['editcertificate'] = '認定証 \'{$a}\' を編集';
+$string['editcontent'] = 'コンテンツを編集';
+$string['editdetails'] = '詳細を編集';
+$string['editelement'] = '\'{$a}\' を編集';
+$string['editelementname'] = '要素の名前を編集';
+$string['editpage'] = 'ページ {$a} を編集';
+$string['edittemplatename'] = 'テンプレート名を編集';
+$string['elementname'] = '要素名';
+$string['elementname_help'] = 'これは、認定証を編集するときにこの要素を識別するために使われる名前です。これはPDFに表示されないので注意してください。';
+$string['elementwidth'] = '幅(mm)';
+$string['elementwidth_help'] = '要素の幅を設定します。0のときは特に幅の設定がないことを示します。';
+$string['entitycertificate'] = '認定証';
+$string['entitycertificateissue'] = '認定証の発行';
+$string['entitycertificateissues'] = '認定証の発行';
+$string['errornopermissionissuecertificate'] = 'この認定証を発行する権限がありません';
+$string['eventcertificateissued'] = '認定証発行日';
+$string['eventcertificaterevoked'] = '認定証の失効';
+$string['eventcertificateverified'] = '認定証の確認';
+$string['eventtemplatecreated'] = '作成済みテンプレート';
+$string['eventtemplatedeleted'] = '削除されたテンプレート';
+$string['eventtemplateupdated'] = '更新されたテンプレート';
+$string['expired'] = '有効期限切れ';
+$string['expiredcertificate'] = 'この認定証の有効期限が切れています';
+$string['expires'] = '有効期限切れ';
+$string['font'] = 'フォント';
+$string['font_help'] = '要素を作成するとき利用するフォント';
+$string['fontcolour'] = '色';
+$string['fontcolour_help'] = 'フォントの色';
+$string['fontsize'] = 'サイズ(pt)';
+$string['fontsize_help'] = 'フォントのサイズ(ポイント)';
+$string['hideshow'] = '非表示/表示';
+$string['invalidcolour'] = '無効な色が選択されています。有効なHTMLカラーコードか、6桁あるいは3桁の16進数カラーコードを入力してください。';
+$string['invalidelementwidth'] = '正の数を入力してください。';
+$string['invalidheight'] = '高さは0以上の有効な数字である必要があります。';
+$string['invalidmargin'] = '余白は0以上の有効な数字である必要があります。';
+$string['invalidposition'] = 'ポジション {$a} には正の数を選択してください。';
+$string['invalidwidth'] = '幅は0以上の有効な数字である必要があります。';
+$string['issuecertificates'] = '新しい認定証を発行する';
+$string['issuedon'] = '発行日';
+$string['issuenewcertificate'] = 'このテンプレートで新しい認定証を発行する';
+$string['issuenewcertificates'] = '新しい認定証を発行する';
+$string['issuenotallowed'] = 'このテンプレートで認定証を発行することはできません。';
+$string['issueormangenotallowed'] = 'このテンプレートで認定証を発行したり、管理することはできません。';
+$string['leftmargin'] = '左余白(mm)';
+$string['leftmargin_help'] = 'これは認定証PDFの左余白(mm)です。';
+$string['manageelementplugins'] = '認定証要素プラグインを管理';
+$string['managetemplates'] = '認定証テンプレートを管理';
+$string['mappingerrorcertificateheader'] = '一部の認定証テンプレートが存在しません';
+$string['mappingerrorcertificatelog'] = '認定証 {$a} が見つかりません';
+$string['messageprovider:certificateissued'] = '認定証を受け取りました';
+$string['mycertificates'] = '私の認定証';
+$string['mycertificatesdescription'] = 'これは学習者がメールか手動ダウンロードで発行した認定証です。';
+$string['name'] = '名前';
+$string['nametoolong'] = '名前の文字数が制限を超えています。';
+$string['noimage'] = '画像がありません';
+$string['noissueswerecreated'] = '発行されていません';
+$string['nopermissionform'] = 'このフォームにアクセスする権限がありません。';
+$string['notificationmsgcertificateissued'] = '{$a->fullname}さん、<br /><br />認定証が利用可能になりました！ URL:
+<a href="{$a->url}">私の認定証</a>';
+$string['notificationsubjectcertificateissued'] = 'あなたの証明書が利用可能です！';
+$string['notverified'] = '未確認';
+$string['numberofpages'] = 'ページ数';
+$string['oneissuewascreated'] = '1枚発行されました';
+$string['outcomecertificate'] = '認定証を発行';
+$string['outcomecertificatedescription'] = '認定証 \'{$a}\' をユーザに発行する';
+$string['page'] = 'ページ {$a}';
+$string['pageheight'] = 'ページの高さ(mm)';
+$string['pageheight_help'] = 'これは認定証PDFの高さ(mm)です。参考までに、A4サイズの高さが297mm、レターサイズの高さが279mmです。';
+$string['pagewidth'] = 'ページ幅(mm)';
+$string['pagewidth_help'] = 'これは認定証PDFの幅(mm)です。参考までに、A4サイズの高さが297mm、レターサイズの高さが279mmです。';
+$string['pluginname'] = '認定証ツール';
+$string['posx'] = 'ポジションX(mm)';
+$string['posx_help'] = 'これはポジションX(mm)で、左上の隅からx軸(横軸)方向に指定した距離だけ離れて要素が配置されます。';
+$string['posy'] = 'ポジションY(mm)';
+$string['posy_help'] = 'これはポジションY(mm)で、左上の隅からy軸(縦軸)方向に指定した距離だけ離れて要素が配置されます。';
+$string['privacy:metadata:tool_certificate:issues'] = '発行済みの認定証の一覧';
+$string['privacy:metadata:tool_certificate_issues:code'] = '認定証に付属するコード';
+$string['privacy:metadata:tool_certificate_issues:expires'] = '認定証の有効期限日です。有効期限がない場合は0を入力します。';
+$string['privacy:metadata:tool_certificate_issues:templateid'] = '認定証のID';
+$string['privacy:metadata:tool_certificate_issues:timecreated'] = '認定証発行日';
+$string['privacy:metadata:tool_certificate_issues:userid'] = '認定証を発行したユーザのID';
+$string['receiveddate'] = '取得者';
+$string['reg_wpcertificates'] = '認定証({$a})の数';
+$string['reg_wpcertificatesissues'] = '発行済みの認定証({$a})の数';
+$string['regenerate'] = '再発行';
+$string['regeneratefileconfirm'] = 'このユーザーに発行された認定証を再発行してもよろしいですか？';
+$string['regenerateissuefile'] = '発行ファイルを再発行する';
+$string['revoke'] = '取り消し';
+$string['revokecertificateconfirm'] = '本当にこのユーザからの認定証発行を取り消しますか？';
+$string['rightmargin'] = '右余白(mm)';
+$string['rightmargin_help'] = 'これは認定証PDFの右余白(mm)です';
+$string['selectcertificate'] = '認定証を選択';
+$string['selectuserstoissuecertificatefor'] = '認定証を発行するユーザを選択';
+$string['shared'] = '共有';
+$string['subplugintype_certificateelement_plural'] = '要素プラグイン';
+$string['timecreated'] = '作成日時';
+$string['toomanycertificatestoshow'] = '認定証({$a})が多すぎて表示できません';
+$string['type'] = 'タイプ';
+$string['uploadimage'] = '画像をアップロード';
+$string['valid'] = '有効';
+$string['validcertificate'] = 'この認定証は有効です';
+$string['verified'] = '確認済み';
+$string['verify'] = '確認';
+$string['verifycertificates'] = '認定証を確認する';
+$string['verifynotallowed'] = '認定証を確認ができません';
+$string['viewcertificate'] = '認定証を表示';
