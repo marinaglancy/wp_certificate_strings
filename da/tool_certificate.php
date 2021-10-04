386132ef70b15f216cd18aed1dc598e5f7784834
Marina Glancy <marina@moodle.com>
2021-10-04 10:58:12 +0200

    da

---
+++ da/tool_certificate.php
@@ -0,0 +1,189 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_certificate', language 'da_wp', version '3.11'.
+ *
+ * @package     tool_certificate
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addcertpage'] = 'Ny side';
+$string['addelement'] = 'Nyt element';
+$string['addelementwithname'] = 'Tilføj et  \'{$a}\'  element';
+$string['aissueswerecreated'] = '{$a} blev udstedt';
+$string['aligncentre'] = 'Centrer';
+$string['alignleft'] = 'Venstre';
+$string['alignment'] = 'Tekst justering';
+$string['alignment_help'] = 'Højre justering af teksten betyder at elementernes koordinater (x og y-akser) bliver sat med reference til det øverste højre hjørne at teksteboksen. Ved centreret justering bliver de sat med reference til midten og ved venstre justering i forhold til det øverste venstre hjørne.';
+$string['alignright'] = 'Højre';
+$string['availableincourses'] = 'Tilgængelig i underkategorier og kurser';
+$string['availableincourses_help'] = 'Ved at slå denne indstilling til, kan brugere (med retten til at udstede) bruge denne skabelon i alle kurser i den valgte kategori og i underkategorierne. Hvis indstillingen er slå fra, er skabelonen kan tilgængelig til brugere (med retten til at udstede) i den valgte kategori.';
+$string['certificate'] = 'Kategori';
+$string['certificate:image'] = 'Administrer billeder på certifikater';
+$string['certificate:issue'] = 'Udsted certifikater til brugeere';
+$string['certificate:manage'] = 'Administrer certifikater';
+$string['certificate:verify'] = 'Verificer alle certifikater';
+$string['certificate:viewallcertificates'] = 'Vis alle udstedte certifikater og skabeloner';
+$string['certificate_customfield'] = 'Tilpassede certifikatfelter';
+$string['certificatecopy'] = '{$a} (kopier)';
+$string['certificateimages'] = 'Certifikatbilleder';
+$string['certificates'] = 'Certifikater';
+$string['certificatesissued'] = 'Udstedte certifikater';
+$string['certificatesissues'] = 'Certifikat udstedelser';
+$string['certificatetemplate'] = 'Certifikatskabelon';
+$string['certificatetemplatename'] = 'Certifikateskabelonnavn';
+$string['certificatetemplates'] = 'Certifikatskabeloner';
+$string['changeelementsequence'] = 'Flyt fremad eller bagud';
+$string['code'] = 'Kode';
+$string['codewithlink'] = 'Kode med link';
+$string['coursecategorywithlink'] = 'Kursuskategori med link';
+$string['createtemplate'] = 'Ny certifikatskabelon';
+$string['customfield_previewvalue'] = 'Previewværdi';
+$string['customfield_previewvalue_help'] = 'Værdi som vises når certifikat vises i preview';
+$string['customfield_visible'] = 'Synlig';
+$string['customfield_visible_help'] = 'Tillad at dette felt vælges på certifikatskabelonen';
+$string['customfieldsettings'] = 'Fælles indstillinger for tilpassede certifikatfelter';
+$string['deleteelement'] = 'Slet element';
+$string['deleteelementconfirm'] = 'Er du sikker på at du vil slette dette element  \'{$a}\'?';
+$string['deletepage'] = 'Slet side';
+$string['deletepageconfirm'] = 'Er du sikker på at du vil slette denne certifikat side?';
+$string['deletetemplateconfirm'] = 'Er du sikker på at du vil slette denne certifikatskabelon  \'{$a}\' og alle tilknyttede data? Du kan ikke fortryde denne handling.';
+$string['demotmpl'] = 'Demo certifikatskabelon';
+$string['demotmplawardedon'] = 'Tildel på';
+$string['demotmplawardedto'] = 'Dette certifikat er tildelt til';
+$string['demotmplbackground'] = 'Baggrundsbillede';
+$string['demotmplcoursefullname'] = 'Kursets fulde navn';
+$string['demotmpldirector'] = 'Skoldeleder';
+$string['demotmplforcompleting'] = 'For at fuldføre dette kursus';
+$string['demotmplissueddate'] = 'Udstedt dato';
+$string['demotmplqrcode'] = 'QR kode';
+$string['demotmplsignature'] = 'Underskrift';
+$string['demotmplusername'] = 'Brugernavn';
+$string['duplicate'] = 'Kopier';
+$string['duplicatetemplateconfirm'] = 'Er du sikker på at du vil kopiere denne skabelon \'{$a}\'?';
+$string['editcertificate'] = 'Rediger certifikatskabelon \'{$a}\'';
+$string['editcontent'] = 'Rediger indhold';
+$string['editdetails'] = 'Rediger detaljer';
+$string['editelement'] = 'Rediger \'{$a}\'';
+$string['editelementname'] = 'Redigere elementets navn';
+$string['editpage'] = 'Rediger side {$a}';
+$string['edittemplatename'] = 'Rediger skabelonnavn';
+$string['elementname'] = 'Elementnavn';
+$string['elementname_help'] = 'Det navn vil blive brugt til at identificere elementet, når du redigere et certifikat. Bemærk at dette ikke vises i PDF\'en.';
+$string['elementwidth'] = 'Bredde';
+$string['elementwidth_help'] = 'Angive bredden på elementet. Nul angiver at der ikke er nogen maksimal bredde.';
+$string['entitycertificate'] = 'Certifikat';
+$string['entitycertificateissue'] = 'Certifikat udstedt';
+$string['entitycertificateissues'] = 'Certifikat udstedes';
+$string['errornopermissionissuecertificate'] = 'Du har ikke rettigheder til at udsted certifikatet';
+$string['eventcertificateissued'] = 'Certifikat udstedt';
+$string['eventcertificaterevoked'] = 'Certifikat tilbagekaldt';
+$string['eventcertificateverified'] = 'Certifikat bekræftet';
+$string['eventtemplatecreated'] = 'Skabelon oprettet';
+$string['eventtemplatedeleted'] = 'Skabelon slettet';
+$string['eventtemplateupdated'] = 'Skabelon opdateret';
+$string['expired'] = 'Udløbet';
+$string['expiredcertificate'] = 'Dette certifikat er udløbet';
+$string['expires'] = 'Udløber den';
+$string['font'] = 'Skrifttype';
+$string['font_help'] = 'Skrifttypen som bruges når dette element genereres';
+$string['fontcolour'] = 'Farve';
+$string['fontcolour_help'] = 'Farven for skrifttypen';
+$string['fontsize'] = 'Størrelse';
+$string['fontsize_help'] = 'Skriftstørrelsne i points';
+$string['hideshow'] = 'Vis/skjul';
+$string['invalidcolour'] = 'Ugyldig farveværdi. Indtast et gyldigt HTML farvenavn, en 6-cifret eller 3-cifret hexadecimal farvekode.';
+$string['invalidelementwidth'] = 'Indtast et positivt tal';
+$string['invalidheight'] = 'Højden skal være et tal større end nul';
+$string['invalidmargin'] = 'Marginnen skal være et tal større end nul';
+$string['invalidposition'] = 'Angiv et positivt tal for position {$a}';
+$string['invalidwidth'] = 'Bredden skal være et tal større en nul';
+$string['issuecertificates'] = 'Udsted certifikater';
+$string['issuedon'] = 'Udstedt på';
+$string['issuenewcertificate'] = 'Certifikater udstedt på baggrund af denne skabelon';
+$string['issuenewcertificates'] = 'Udsted nye certifikater';
+$string['issuenotallowed'] = 'Du må ikke udstede certifikater på baggrund af denne skabelon.';
+$string['issueormangenotallowed'] = 'Du må hverken udstede certifikater på baggrund af denne skabelon eller redigere skabelonen.';
+$string['leftmargin'] = 'Venstre margin';
+$string['leftmargin_help'] = 'Dette er venstre margin i certifikat PDF\'en i mm';
+$string['manageelementplugins'] = 'Administrer certifikatelementplugins';
+$string['managetemplates'] = 'Administrer certifikatskabeloner';
+$string['mappingerrorcertificateheader'] = 'Nogle certifikatskabeloner eksisterer ikke';
+$string['mappingerrorcertificatelog'] = 'Certifikatet  {$a} findes ikke';
+$string['messageprovider:certificateissued'] = 'Certifikat modtages';
+$string['milimeter'] = 'mm';
+$string['mycertificates'] = 'Mine certifikater';
+$string['mycertificatesdescription'] = 'Dette er de certifikater du har modtaget enten på mail eller downloadet manuelt.';
+$string['name'] = 'Navn';
+$string['nametoolong'] = 'Du har overskredet den maksimal længde for navn';
+$string['noimage'] = 'Intet billede';
+$string['noissueswerecreated'] = 'Intet certifikat blev udstedt';
+$string['nopermissionform'] = 'Du har ikke tilladelse til at tilgå denne formular';
+$string['notificationmsgcertificateissued'] = 'Hej {$a->fullname},<br /><br />Dit certifikat er nu klar! Du kan finde det her:
+<a href="{$a->url}">Mine certifikater</a>';
+$string['notificationsubjectcertificateissued'] = 'Dit certifikat er klar!';
+$string['notverified'] = 'Ikke bekræftet';
+$string['numberofpages'] = 'Antal sider';
+$string['oneissuewascreated'] = 'En udstedelse blev oprettet';
+$string['outcomecertificate'] = 'Udsted certifikat';
+$string['outcomecertificatedescription'] = 'Udsted certifikat \'{$a}\' til brugere';
+$string['page'] = 'Side \'{$a}\'';
+$string['pageheight'] = 'Sidehøjde';
+$string['pageheight_help'] = 'Dette er højden af certifikat PDF\'en i mm. Til reference er et A4-papir 297 mm højt.';
+$string['pagewidth'] = 'Sidebredde';
+$string['pagewidth_help'] = 'Dette er bredden af certifikat PDF\'en i mm. Til reference er et A4-papir 210 mm bredt.';
+$string['pluginname'] = 'Certifikatadministration';
+$string['point'] = 'pt';
+$string['posx'] = 'Position X';
+$string['posx_help'] = 'Dette er positionen i mm fra det øverste venstre hjørne, som du ønsker at anvende som elements reference punkt i x-retningen.';
+$string['posy'] = 'Position Y';
+$string['posy_help'] = 'Dette er positionen i mm fra det øverste venstre hjørne, som du ønsker at anvende som elements reference punkt i y-retningen.';
+$string['privacy:metadata:tool_certificate:issues'] = 'Listen af udstedte certifikater';
+$string['privacy:metadata:tool_certificate_issues:code'] = 'Koden som angives på certifikatet';
+$string['privacy:metadata:tool_certificate_issues:expires'] = 'Tidsstempel for hvornår certifikatet udløber. 0 angiver at det ikke udløber';
+$string['privacy:metadata:tool_certificate_issues:templateid'] = 'Certifikatets id';
+$string['privacy:metadata:tool_certificate_issues:timecreated'] = 'Tidspunkt for certifikatudstedelse';
+$string['privacy:metadata:tool_certificate_issues:userid'] = 'Id på brugeren som udstedte certifikatet';
+$string['receiveddate'] = 'Tildelt på';
+$string['reg_wpcertificates'] = 'Antal certifikater ({$a})';
+$string['reg_wpcertificatesissues'] = 'Antal af udstedte certifikater ({$a})';
+$string['regenerate'] = 'Regenerer';
+$string['regeneratefileconfirm'] = 'Er du sikker på at du vil regenere certifikatet udstedt til denne bruger?';
+$string['regenerateissuefile'] = 'Regenerer udstedelsesfil';
+$string['revoke'] = 'Tilbagekald';
+$string['revokecertificateconfirm'] = 'Er du sikker på at du vil tilbagekalde certifikatet fra denne bruger?';
+$string['rightmargin'] = 'Højre margin';
+$string['rightmargin_help'] = 'Dette er høre margin i certifikat PDF\'en';
+$string['selectcertificate'] = 'Vælg certifikat';
+$string['selectuserstoissuecertificatefor'] = 'Vælg brugere at udstede certifikat til';
+$string['shared'] = 'Delt';
+$string['subplugintype_certificateelement_plural'] = 'Elementplugins';
+$string['timecreated'] = 'Tidspunkt for oprettelse';
+$string['toomanycertificatestoshow'] = 'For mange certifikater ({$a}) at vise';
+$string['type'] = 'Type';
+$string['uploadimage'] = 'Opload billede';
+$string['valid'] = 'Gyldig';
+$string['validcertificate'] = 'Dette certifikat er gyldigt';
+$string['verified'] = 'Bekræftet';
+$string['verify'] = 'Bekræft';
+$string['verifycertificates'] = 'Bekræft certifikater';
+$string['verifynotallowed'] = 'Du har ikke ret til at bekræfte certifikater';
+$string['viewcertificate'] = 'Vis certifikat';
